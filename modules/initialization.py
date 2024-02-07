594c58a6a248f3d61253340335c717a813d0b6e2
Jason Stiles <25235048+vertigozero1@users.noreply.github.com>
2024-02-07 16:32:40 -0600

    improved compliance to code standards

--- modules/initialization.py
+++ modules/initialization.py
@@ -1,11 +1,10 @@
 import configparser # for reading the config file
 import logging      # for logging errors
 import traceback    # for printing exceptions
-import logging      # for logging errors
 import sys          # for logging to stdout
 
-def interpretLogLevel(logLevel):
-    """Given a log level string, returns the corresponding log level integer from the logging library"""
+def interpret_log_level(logLevel):
+    """ Given a log level string, returns the corresponding log level integer from the logging library """
     return {
         "DEBUG": logging.DEBUG,
         "INFO": logging.INFO,
@@ -15,12 +14,12 @@ def interpretLogLevel(logLevel):
         }.get(logLevel.upper(), logging.WARNING) # default to WARNING if the log level is not recognized
 
 
-def getConfig():
-    """Import the config file and return the data object"""
+def get_config():
+    """ Import the config file and return the data object """
     try:
-        rawConfig = configparser.ConfigParser()
-        rawConfig.read('config.ini')
-    except:
+        raw_config = configparser.ConfigParser()
+        raw_config.read('config.ini')
+    except FileNotFoundError:
         print("Error reading config file")
         traceback.print_exc()
         exit(1)
@@ -28,25 +27,25 @@ def getConfig():
     try:
         class config:
             def __init__(self):
-                configLogLevel = rawConfig.get('APPLICATION', 'logLevel', fallback='WARNING')
-                self.logLevel = interpretLogLevel(configLogLevel)
+                configLogLevel = raw_config.get('APPLICATION', 'logLevel', fallback='WARNING')
+                self.logLevel = interpret_log_level(configLogLevel)
 
-                self.apiKey = rawConfig['OPENWEATHER']['apiKey']
+                self.apiKey = raw_config['OPENWEATHER']['apiKey']
                 
-                self.units = rawConfig['OPENWEATHER']['units']            
+                self.units = raw_config['OPENWEATHER']['units']            
 
-                self.city1Name = rawConfig['OPENWEATHER']['city1Name']
-                self.city1Lati = rawConfig['OPENWEATHER']['city1Lati']
-                self.city1Long = rawConfig['OPENWEATHER']['city1Long']
+                self.city1Name = raw_config['OPENWEATHER']['city1Name']
+                self.city1Lati = raw_config['OPENWEATHER']['city1Lati']
+                self.city1Long = raw_config['OPENWEATHER']['city1Long']
 
-                if rawConfig['OPENWEATHER']['city2Lati'] != 'latitude':
+                if raw_config['OPENWEATHER']['city2Lati'] != 'latitude':
                     self.mode = 'dual'
-                    self.city2Name = rawConfig['OPENWEATHER']['city2Name']
-                    self.city2Lati = rawConfig['OPENWEATHER']['city2Lati']
-                    self.city2Long = rawConfig['OPENWEATHER']['city2Long']
+                    self.city2Name = raw_config['OPENWEATHER']['city2Name']
+                    self.city2Lati = raw_config['OPENWEATHER']['city2Lati']
+                    self.city2Long = raw_config['OPENWEATHER']['city2Long']
                 else:
                     self.mode = 'single'
-    except:
+    except Exception:
         print("Error parsing config file")
         traceback.print_exc()
         exit(1)
@@ -55,7 +54,7 @@ def getConfig():
     return output
 
 
-def startLogging(logLevel):
+def start_logging(logLevel):
     """Set up logging for the main application, with a log object that can be passed to methods"""
     class track:
         def __init__(self):
