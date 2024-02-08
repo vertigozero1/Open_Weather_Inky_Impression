f4be0ae9c24284af40f217deb7123e30cb61864b
vertigozero1 <vertigozero1@users.noreply.github.com>
2024-02-07 22:21:50 -0600

    Refactor variable names and fix typos in initialization.py

--- modules/initialization.py
+++ modules/initialization.py
@@ -3,7 +3,7 @@ import logging      # for logging errors
 import traceback    # for printing exceptions
 import sys          # for logging to stdout
 
-def interpret_log_level(logLevel):
+def interpret_log_level(log_level):
     """ Given a log level string, returns the corresponding log level integer from the logging library """
     return {
         "DEBUG": logging.DEBUG,
@@ -11,7 +11,7 @@ def interpret_log_level(logLevel):
         "WARNING": logging.WARNING,
         "ERROR": logging.ERROR,
         "CRITICAL": logging.CRITICAL
-        }.get(logLevel.upper(), logging.WARNING) # default to WARNING if the log level is not recognized
+        }.get(log_level.upper(), logging.WARNING) # default to WARNING if the log level is not recognized
 
 
 def get_config():
@@ -27,22 +27,23 @@ def get_config():
     try:
         class config:
             def __init__(self):
-                configLogLevel = raw_config.get('APPLICATION', 'logLevel', fallback='WARNING')
-                self.logLevel = interpret_log_level(configLogLevel)
+                config_log_level = raw_config.get('APPLICATION', 'logLevel', fallback='WARNING')
+                self.log_evel = interpret_log_level(config_log_level)
 
-                self.apiKey = raw_config['OPENWEATHER']['apiKey']
-                
-                self.units = raw_config['OPENWEATHER']['units']            
+                self.api_key = raw_config['OPENWEATHER']['apiKey']
 
-                self.city1Name = raw_config['OPENWEATHER']['city1Name']
-                self.city1Lati = raw_config['OPENWEATHER']['city1Lati']
-                self.city1Long = raw_config['OPENWEATHER']['city1Long']
+                self.units = raw_config['OPENWEATHER']['units']
+                self.render_method = raw_config['APPLICATION']['renderMethod']
+
+                self.city_one_name = raw_config['OPENWEATHER']['city1Name']
+                self.city_one_lat = raw_config['OPENWEATHER']['city1Lati']
+                self.city_one_lon = raw_config['OPENWEATHER']['city1Long']
 
                 if raw_config['OPENWEATHER']['city2Lati'] != 'latitude':
                     self.mode = 'dual'
-                    self.city2Name = raw_config['OPENWEATHER']['city2Name']
-                    self.city2Lati = raw_config['OPENWEATHER']['city2Lati']
-                    self.city2Long = raw_config['OPENWEATHER']['city2Long']
+                    self.city_two_name = raw_config['OPENWEATHER']['city2Name']
+                    self.city_two_lat = raw_config['OPENWEATHER']['city2Lati']
+                    self.city_two_lon = raw_config['OPENWEATHER']['city2Long']
                 else:
                     self.mode = 'single'
     except Exception:
@@ -54,25 +55,26 @@ def get_config():
     return output
 
 
-def start_logging(logLevel):
+def start_logging(log_level):
     """Set up logging for the main application, with a log object that can be passed to methods"""
     class track:
+        """ A class to hold the logger and handlers for application runtime data tracking """
         def __init__(self):
             self.logger = logging.getLogger()
-            self.logger.setLevel(logLevel)
+            self.logger.setLevel(log_level)
 
-            self.fileHandler = logging.FileHandler('weatherDisplay.log')
-            self.fileHandler.setLevel(logLevel)
+            self.file_handler = logging.FileHandler('weatherDisplay.log')
+            self.file_handler.setLevel(log_level)
 
-            self.stdoutHandler = logging.StreamHandler(sys.stdout)
-            self.stdoutHandler.setLevel(logLevel)
+            self.stdout_handler = logging.StreamHandler(sys.stdout)
+            self.stdout_handler.setLevel(log_level)
 
             self.formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
 
-            self.fileHandler.setFormatter(self.formatter)
-            self.stdoutHandler.setFormatter(self.formatter)
+            self.file_handler.setFormatter(self.formatter)
+            self.stdout_handler.setFormatter(self.formatter)
 
-            self.logger.addHandler(self.fileHandler)
-            self.logger.addHandler(self.stdoutHandler)
+            self.logger.addHandler(self.file_handler)
+            self.logger.addHandler(self.stdout_handler)
     output = track()
     return output
