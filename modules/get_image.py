5eb9f8fab652e3a4014753fc69ece2148d99a047
vertigozero1 <vertigozero1@users.noreply.github.com>
2024-02-08 21:21:47 -0600

    added PIL rendering option

--- modules/get_image.py
+++ modules/get_image.py
@@ -2,6 +2,9 @@ import imgkit                       # for html to image conversion
 from html2image import Html2Image   # alternate for html to image conversion
 import traceback                    # for error handling
 import sys                          # for error handling
+from PIL import Image
+from inky.auto import auto
+from PIL import Image,ImageDraw,ImageFont
 
 def render_imgkit(out):
     """ Render HTML to image using imgkit """
@@ -21,6 +24,59 @@ def render_html2image(html, out):
         out.logger.critical("Error rendering HTML to image")
         out.logger.critical(traceback.format_exc())
         sys.exit
+
+
+def render_pil():
+    """ Render text to image using PIL """
+    ''' Urbanist-Thin.ttf
+            Urbanist-ThinItalic.ttf
+        Urbanist-ExtraLight.ttf
+            Urbanist-ExtraLightItalic.ttf
+        Urbanist-Light.ttf
+            Urbanist-LightItalic.ttf
+        Urbanist-Regular.ttf
+            Urbanist-Italic.ttf
+        Urbanist-Medium.ttf
+            Urbanist-MediumItalic.ttf
+        Urbanist-SemiBold.ttf
+            Urbanist-SemiBoldItalic.ttf
+        Urbanist-Bold.ttf
+            Urbanist-BoldItalic.ttf
+        Urbanist-ExtraBold.ttf
+            Urbanist-ExtraBoldItalic.ttf
+        Urbanist-Black.ttf
+            Urbanist-BlackItalic.ttf
+       '''
+    unicode_text = u"Hello World!"
+    
+    # get the line size
+    text_width, text_height = font.getsize(unicode_text)
+
+    # create a blank canvas with extra space between lines
+    canvas = Image.new('RGB', (text_width + 10, text_height + 10), "orange")
+
+    # draw the text onto the text canvas, and use blue as the text color
+    draw = ImageDraw.Draw(canvas)
+    
+    font = ImageFont.truetype("/usr/share/fonts/truetype/Urbanist-Black.ttf", 32, encoding="unic")
+    draw.text((5,5), u'URBANIST black, in blue', 'blue', font)
+
+    font = ImageFont.truetype("/usr/share/fonts/truetype/Urbanist-ExtraBoldItalic.ttf", 20, encoding="unic")
+    draw.text((5,40), u'URBANIST extraBoldItalic, in orange', 'orange', font)
+
+    # save the blank canvas to a file
+    canvas.save("pil-text.png", "PNG")
+
+    inky = auto(ask_user=True, verbose=True)
+    saturation = 0.5
+
+    image = Image.open("pil-text.png")
+    resizedimage = image.resize(inky.resolution)
+
+    inky.set_image(resizedimage, saturation=saturation)
+    canvas.show()
+    inky.show()
+
 
 def image_example():
     """ HTML2IMG REQUIRES CHROME TO BE INSTALLED ON THE SYSTEM
