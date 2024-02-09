13fb64ea72570045bc620e873b583df2a8cd6d94
vertigozero1 <vertigozero1@users.noreply.github.com>
2024-02-09 17:06:42 -0600

    sanity check

--- modules/weather.py
+++ modules/weather.py
@@ -83,7 +83,7 @@ def create_data_object(name, data):
                 self.icon =  'wi-day-sunny'
             elif self.id > 800:
                 self.icon =  'wi-cloudy'
-    summary = data['daily'][0]['summary']
+    summary = data['current']['temp']
     weather_id = data['current']['weather'][0]['id']
     weather = data['current']['weather'][0]['description']
     temp = format_temp(data['current']['temp'])
